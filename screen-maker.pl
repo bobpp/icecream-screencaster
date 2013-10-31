9ccfb5f0b5f42253f883e9f67d299879796cbfa8
bobpp <bobpp.asroma+github@gmail.com>
2013-11-01 02:37:22 +0900

    Not html fetch at Tweet-ID = DEFAULT

--- screen-maker.pl
+++ screen-maker.pl
@@ -7,23 +7,30 @@ use Furl;
 use Path::Tiny;
 use URI;
 use Data::Dumper;
+use Getopt::Long;
+
+GetOptions('theme=i' => \my $theme);
+
+my $stash = { theme_id => $theme };
 
 my($id) = @ARGV;
+unless ($id eq 'DEFAULT') {
+	my $uri = URI->new('https://api.twitter.com/1/statuses/oembed.json');
+	$uri->query_form(id => $id, maxwidth => 550, hide_media => 'true', hide_thread => 'true', omit_script => 'true', align => 'center');
 
-my $uri = URI->new('https://api.twitter.com/1/statuses/oembed.json');
-$uri->query_form(id => $id, maxwidth => 550, hide_media => 'true', hide_thread => 'true', omit_script => 'true', align => 'center');
+	my $ua = Furl->new(timeout => 3);
+	my $twitter_api_response = $ua->get($uri->as_string);
+	if ($twitter_api_response->is_success) {
+		my $result = decode_json($twitter_api_response->body);
+		$stash->{tweet_html} = $result->{html};
+	}
+	else {
+		print Dumper($twitter_api_response);
+		die;
+	}
+}
 
-my $ua = Furl->new(timeout => 3);
-my $twitter_api_response = $ua->get($uri->as_string);
-if ($twitter_api_response->is_success) {
-	my $result = decode_json($twitter_api_response->body);
-	my $tx = Text::Xslate->new(
-		path => 'views',
-	);
-	my $path = Path::Tiny->new('htdocs/screen.html');
-	$path->spew_utf8($tx->render('tweet.tx', { tweet_html => $result->{html} }));
-}
-else {
-	print Dumper($twitter_api_response);
-}
+Path::Tiny->new('htdocs/screen.html')->spew_utf8(
+	Text::Xslate->new(path => 'views')->render('tweet.tx', $stash)
+);
 
